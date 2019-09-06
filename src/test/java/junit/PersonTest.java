22af6f7471caa53aaab3f628dd845069aa763e24
paragk05 <paragk106@gmail.com>
2019-09-06 12:23:51 +0530

    Java Tutorials: lesson 7

--- src/test/java/junit/PersonTest.java
+++ src/test/java/junit/PersonTest.java
@@ -28,9 +28,49 @@ public class PersonTest
         Person person2=new Person();
         Person person3=new Person();
         Person person4=new Person();
-
-
         assertEquals(4,Person.numberOfPersons());
 
+    }
+
+    @Test
+    public void demonstrateBoolean()
+    {
+        /*
+        boolean monday=false;
+        boolean raining=true;
+        boolean tuesday=true;
+        boolean sunny=false;
+        boolean friday=true;
+
+
+        if ((monday && raining) || (tuesday && sunny))
+        {
+            drinkFizz();
+        }
+        else if(friday)
+        {
+          drinkWater();
+        }
+        else
+        {
+            drinkMilk();
+        }
+        */
+
+        int i=4,j=3;
+        boolean monday = true;
+
+        if(i>j)
+        {
+            i=99;
+        }
+        else if(j>i)
+        {
+            i=9;
+        }
+        else if(monday==true)
+        {
+            j--;
+        }
     }
 }
